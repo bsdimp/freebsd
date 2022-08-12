1b2cf5cf891a9746007356d05eae77d9d18d304e
Warner Losh <imp@FreeBSD.org>
2022-08-12 10:43:22 -0600

    stub out arm64 build a bit more

--- stand/kboot/arch/aarch64/load_addr.c
+++ stand/kboot/arch/aarch64/load_addr.c
@@ -1,9 +1,15 @@
 #include <sys/param.h>
 
 #include "kboot.h"
+#include "bootstrap.h"
 
 uint64_t
 kboot_get_phys_load_segment(void)
 {
 	return 0;
+}
+
+void
+bi_loadsmap(struct preloaded_file *kfp)
+{
 }
