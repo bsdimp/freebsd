238c4b3ed3ca12297c8254f6876f05c0cc793e9e
Warner Losh <imp@FreeBSD.org>
2023-04-25 20:16:26 -0600

    lsg: The Lua Syscall Generator Library syscall.master parser
    Parsing code for syscall.master. It reads in FreeBSD's syscall.master
    format and creates a table of syscall objects.
    
    Parts of this code came from Kyle Evans' makesyscall.lua
    
    Sponsored by:		Netflix

---
+++ sys/tools/lsg/freebsd-syscall.lua
@@ -0,0 +1,100 @@
+--
+-- SPDX-License-Identifier: BSD-2-Clause
+--
+-- Copyright (c) 2023 Warner Losh <imp@bsdimp.com>
+--
+
+-- Derived in large part from makesyscalls.lua:
+--
+-- SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+--
+-- Copyright (c) 2019 Kyle Evans <kevans@FreeBSD.org>
+
+local syscall = require("syscall")
+
+local FreeBSDSyscall = {}
+
+FreeBSDSyscall.__index = FreeBSDSyscall
+
+function FreeBSDSyscall:parse_sysfile()
+	local file = self.sysfile
+	local config = self.config
+	local commentExpr = "^%s*;.*"
+
+	if file == nil then
+		print "No file"
+		return
+	end
+
+	self.syscalls = { }
+
+	local fh = io.open(file)
+	if fh == nil then
+		print("Failed to open " .. file)
+		return {}
+	end
+
+	local incs = ""
+	local defs = ""
+	local s
+	for line in fh:lines() do
+		line = line:gsub(commentExpr, "")		-- Strip any comments
+
+		-- Note can't use pure pattern matching here because of the 's' test
+		-- and this is shorter than a generic pattern matching pattern
+		if line == nil or line == "" then
+			-- nothing blank line or end of file
+		elseif s ~= nil then
+			-- If we have a partial system call object
+			-- s, then feed it one more line
+			if s:add(line) then
+				-- append to syscall list
+				for t in s:iter() do
+					table.insert(self.syscalls, t)
+				end
+				s = nil
+			end
+		elseif line:match("^%s*%$") then
+			-- nothing, obsolete $FreeBSD$ thing
+		elseif line:match("^#%s*include") then
+			incs = incs .. line .. "\n"
+		elseif line:match("%%ABI_HEADERS%%") then
+			local h= self.config.abi_headers
+			if h ~= nil and h ~= "" then
+				incs = incs .. h .. "\n"
+			end
+		elseif line:match("^#%s*define") then
+			defs = defs .. line.. "\n"
+		elseif line:match("^#") then
+			abort(1, "Unsupported cpp op " .. line)
+		else
+			s = syscall:new()
+			if s:add(line) then
+				-- append to syscall list
+				for t in s:iter() do
+					table.insert(self.syscalls, t)
+				end
+				s = nil
+			end
+		end
+	end
+	if s ~= nil then
+		abort(1, "Dangling system call at the end")
+	end
+
+	assert(io.close(fh))
+	self.includes = incs
+	self.defines = defs
+end
+
+function FreeBSDSyscall:new(obj)
+	obj = obj or { }
+	setmetatable(obj, self)
+	self.__index = self
+
+	obj:parse_sysfile()
+
+	return obj
+end
+
+return FreeBSDSyscall
