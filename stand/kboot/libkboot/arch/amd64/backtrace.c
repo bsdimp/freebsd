353404afd41b712fd047fd8d9ce603b7c047becf
Warner Losh <imp@FreeBSD.org>
2024-11-21 16:44:13 -0700

    kboot: Add CPU state dump for amd64
    Sponsored by:		Netflix

--- stand/kboot/libkboot/arch/amd64/backtrace.c
+++ stand/kboot/libkboot/arch/amd64/backtrace.c
@@ -5,12 +5,33 @@
  */
 
 #include "backtrace.h"
+#include "signal.h"
+#include "stand.h"
 
 extern void *stack_upper_limit;
+
+struct amd64_frame
+{
+	struct amd64_frame *frame;
+	uint64_t rip;
+};
 
 void
 text_crash_dump_ctx(void *ctx)
 {
+	host_ucontext_t *ucp = ctx;
+	struct amd64_frame *frame = (struct amd64_frame *)ucp->uc_mcontext.gregs[REG_RBP];
+	uintptr_t first = (uintptr_t)frame;
+	uintptr_t upper = (uintptr_t)stack_upper_limit;
+
+	printf("XXX CRASH XXX Should dump the registers too XXX CRASH XXX\n");
+
+	printf("%#llx (frame %p)\n", ucp->uc_mcontext.gregs[REG_RIP], frame);
+
+	while ((uintptr_t)frame >= first && (uintptr_t)frame < upper) {
+		printf("%#lx (frame %p)\n", frame->rip, frame);
+		frame = frame->frame;
+	}
 }
 
 void
